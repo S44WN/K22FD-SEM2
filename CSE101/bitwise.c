33f50444484a56ab1e4d81fa8138c2d8eab4b1cf
Sawan <itissaavn@gmail.com>
2023-02-08 13:01:29 +0530

    float, double diff. ternary op and format specifiers

--- CSE101/bitwise.c
+++ CSE101/bitwise.c
@@ -1,6 +1,6 @@
 // // write all bitewise operators in C
 
-// #include <stdio.h>
+#include <stdio.h>
 
 // int bitwiseStuff(){
 //     int a = 5;
@@ -45,9 +45,74 @@ int eligibleForExam(){
 
 }
 
+//Question 5
+// write a program to calculate the total bill of a student in a stationary shop. 
+//The shop sells pens, books and notebooks. The prices of pens, books and notebooks are 10, 20 and 15 respectively. 
+//If the total bill is greater than 1000, then 10% GST is added to the total bill.
+
+int calculateBill(){
+    // enter no of pens books and notebooks
+    float pens, books, notebooks;
+    printf("Enter the number of pens : ");
+    scanf("%d", &pens);
+
+    printf("Enter the number of books : ");
+    scanf("%d", &books);
+
+    printf("Enter the number of notebooks : ");
+    scanf("%d", &notebooks);
+
+    // calculate the total bill
+    float totalBill = (pens*10) + (books*20) + (notebooks*15);
+
+    // calculate the gst 10% if total bill is greater than 1000
+
+    float gst = (totalBill > 1000) ? (totalBill*0.1) : 0;
+
+    //update total bill
+    totalBill += gst;
+
+    // print the total bill
+    printf("Total bill is : %f", totalBill);
+
+}
+
+// Question 6
+// write a program to check of you passed ot failed in a subject based on the following criteria:
+// 1. you should have scored 40% in the CA 1 exam
+// 2. you should have scored 40% in the CA 2 exam
+// 2. you should have scored 40% in the mid term exam
+// 3. you should have scored 40% in the total of CA, mid term and final exam
+// 4. you should have attended 75% of the classes
+
+int passOrFail(){
+    int ca1, ca2, mid, total, attended;
+    printf("Enter the CA 1 score : ");
+    scanf("%d", &ca1);
+
+    printf("Enter the CA 2 score : ");
+    scanf("%d", &ca2);
+
+    printf("Enter the MTE score : ");
+    scanf("%d", &mid);
+
+    printf("Enter the ETE score : ");
+    scanf("%d", &total);
+
+    printf("Enter the number of classes attended : ");
+    scanf("%d", &attended);
+
+    (ca1 >= 40 && ca2 >= 40 && mid >= 40 && total >= 40 && attended >= 75) ? printf("Passed") : printf("Failed");
+
+
+};
+
+
+
 int main(){
     // bitwiseStuff();
-    eligibleForExam();
+    // eligibleForExam();
+    calculateBill();
     return 0;
 };
 
---
+++ CSE101/coversionndFormatSpecifiers.c
@@ -0,0 +1,65 @@
+// %d or %i	It is used to print the signed integer value where signed integer means that the variable can hold both positive and negative values.
+// %u	It is used to print the unsigned integer value where the unsigned integer means that the variable can hold only positive value.
+// %o	It is used to print the octal unsigned integer where octal integer value always starts with a 0 value.
+// %x	It is used to print the hexadecimal unsigned integer where the hexadecimal integer value always starts with a 0x value. In this, alphabetical characters are printed in small letters such as a, b, c, etc.
+// %X	It is used to print the hexadecimal unsigned integer, but %X prints the alphabetical characters in uppercase such as A, B, C, etc.
+// %f	It is used for printing the decimal floating-point values. By default, it prints the 6 values after '.'.
+// %e/%E	It is used for scientific notation. It is also known as Mantissa or Exponent.
+// %g	It is used to print the decimal floating-point values, and it uses the fixed precision, i.e., the value after the decimal in input would be exactly the same as the value in the output.
+// %p	It is used to print the address in a hexadecimal form.
+// %c	It is used to print the unsigned character.
+// %s	It is used to print the strings.
+// %ld	It is used to print the long-signed integer value.
+
+#include <stdio.h>
+
+int formatSpecifier()
+{
+    int a = 10;
+    float b = 10.5;
+    char c = 'A';
+    char d[] = "Hello World";
+    printf("Integer value is %d and %i and %u and %o and %x and %X and %p and %c and %s", a, a, a, a, a, a, &a, c, d); // %d, %i, %u, %o, %x, %X, %p, %c, %s
+    printf(" and %f and %e and %E and %g", b, b, b, b);                                                                // %f, %e, %E, %g
+    return 0;
+}
+
+// Output:
+// Integer value is 10 and 10 and 10 and 12 and a and A and 0x7ffccf0c9a0c and A and Hello World and 10.500000 and 1.050000e+01 and 1.050000E+01 and 10.5
+
+// float and double are two different data types in C. The float data type is used to store the single-precision floating-point values, and the double data type is used to store the double-precision floating-point values. The float data type is used to store the 32-bit floating-point values, and the double data type is used to store the 64-bit floating-point values. The float data type is used to store the 32-bit floating-point values, and the double data type is used to store the 64-bit floating-point values.
+int floatDoubleDifference()
+{
+    float a = 10.512345678;
+    double b = 10.5123456789023456789;
+    printf("Float value is %0.8f and Double value is %0.12lf", a, b);
+    return 0;
+}
+
+// Output:
+// Float value is 10.51234579 and Double value is 10.512345678902
+
+int main()
+{
+    formatSpecifier();
+    floatDoubleDifference();
+    return 0;
+}
+
+// find the largest of three number using ternary operator
+// whta is ternary operator in c
+// ternary operator in c - is a conditional operator that takes three operands.
+// It is also known as the conditional operator. It is used to evaluate a condition and
+// return a value based on the result of the condition. It is also known as the conditional operator.
+// It is used to evaluate a condition and return a value based on the result of the condition.
+// Syntax: (condition) ? expression1 : expression2;
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 20, c = 30;
+    int largest = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+    printf("Largest number is %d", largest);
+    return 0;
+}
