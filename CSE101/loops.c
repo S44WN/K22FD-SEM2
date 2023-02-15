b76a5ca58663bee9b57ff184a80047933bcd1a68
Sawan <itissaavn@gmail.com>
2023-02-15 13:32:12 +0530

    add reversenum, tableofnum, sumofdigits, palindronnum

--- CSE101/loops.c
+++ CSE101/loops.c
@@ -80,6 +80,70 @@ int primeOrNot()
     }
 }
 
+int reverseNum()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    int rev = 0;
+    while (n > 0)
+    {
+        int lastDigit = n % 10;
+        rev = rev * 10 + lastDigit;
+        n = n / 10;
+    }
+    printf("Reverse of the number is %d", rev);
+}
+
+int tableOfnum()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d * %d = %d \n", n, i, n * i);
+    }
+}
+
+int sumOfDigits()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    int sum = 0;
+    while (n > 0)
+    {
+        int lastDigit = n % 10;
+        sum = sum + lastDigit;
+        n = n / 10;
+    }
+    printf("Sum of the digits is %d", sum);
+}
+
+int palindromeNum()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    int rev = 0;
+    int temp = n;
+    while (n > 0)
+    {
+        int lastDigit = n % 10;
+        rev = rev * 10 + lastDigit;
+        n = n / 10;
+    }
+    if (temp == rev)
+    {
+        printf("The number is palindrome");
+    }
+    else
+    {
+        printf("The number is not palindrome");
+    }
+}
+
 int main()
 {
     // whileLoop();
@@ -88,5 +152,10 @@ int main()
     // forEachLoop();
     printf("%d", factorial(5));
     primeOrNot();
+    reverseNum();
+    tableOfnum();
+    sumOfDigits();
+    palindromeNum();
+
     return 0;
 }
