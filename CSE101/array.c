d17505b46067cac6533bb5e1fdc46d64e0d27fc4
Sawan <itissaavn@gmail.com>
2023-03-31 13:45:44 +0530

    array stuff - deletion in array

--- CSE101/array.c
+++ CSE101/array.c
@@ -295,6 +295,28 @@ int insertionInArray()
     }
 }
 
+// deletion in array
+
+int deletionInArray()
+{
+    int a[10] = {1, 2, 3, 4, 5};
+    int size = 5;
+    int index;
+    printf("Enter index: ");
+    scanf("%d", &index);
+
+    for (int i = index; i < size - 1; i++)
+    {
+        a[i] = a[i + 1];
+    }
+    size -= 1;
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", a[i]);
+    }
+}
+
 int main()
 {
     // declareArray();
